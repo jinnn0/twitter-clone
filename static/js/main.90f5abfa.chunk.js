b396e74e034fbb6ef442f3a7d5ca598483efdea3
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-10-30 22:22:10 +0000

    Updates

--- static/js/main.90f5abfa.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptwitter-clone"]=this["webpackJsonptwitter-clone"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(0),r=c(1),s=c.n(r),i=c(3);var o=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("h1",{children:"hello there"})})};c.n(i).a.render(Object(n.jsx)(s.a.StrictMode,{children:Object(n.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.90f5abfa.chunk.js.map
