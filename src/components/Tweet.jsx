d2b406023445617a21a8ceac87e5bd9dacc2c28c
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-11-01 09:16:46 +0000

    Update re-structure Tweet for adding user info

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { FiEdit2 } from 'react-icons/fi';
+import { FaUser } from 'react-icons/fa';
 
 const Tweet = ({ tweet, isCreator, deleteTweet, openEditModal }) => {
   return (
     <li className="tweet">
-      <div className="row-1">{tweet.url && <img src={tweet.url} alt="img" />}</div>{' '}
-      <div className="row-2">
-        <span className="tweet__text">{tweet.text}</span>
-        {isCreator && (
-          <>
-            <RiDeleteBinLine className="icon delete-icon" onClick={() => deleteTweet(tweet.id, tweet.url)} />
-            <FiEdit2 className="icon" onClick={() => openEditModal(tweet)} />
-          </>
-        )}
+      <div className="col-1">
+        <FaUser />
+        {/* <img src="" alt="user-img" className="user-img" /> */}
+      </div>
+
+      <div className="col-2">
+        <div className="user-info">
+          Lex Fridman
+          {isCreator && (
+            <div className="icons">
+              <RiDeleteBinLine
+                className="icon delete-icon"
+                onClick={() => deleteTweet(tweet.id, tweet.url)}
+              />
+              <FiEdit2 className="icon" onClick={() => openEditModal(tweet)} />
+            </div>
+          )}
+        </div>
+
+        <div className="tweet-text">{tweet.text}</div>
+        <div className="tweet-img">{tweet.url && <img src={tweet.url} alt="img" />}</div>
       </div>
     </li>
   );
