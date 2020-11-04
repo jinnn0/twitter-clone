f3d7cf90bff73da4ed24ba5dfb2528a1295d3f73
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-11-04 15:59:06 +0000

    Add ProfileEditModal & nav

---
+++ src/components/ProfileEditModal.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useRef } from 'react';
+import * as AiIcons from 'react-icons/ai';
+import useOnClickOutside from '../hooks/useOnClickOutside';
+
+const ProfileEditModal = ({ currentUser, refreshCurrentUser, setIsEditBtnClick }) => {
+  const [newDisplayName, setNewDisplayName] = useState(currentUser.displayName || '');
+  const editFormRef = useRef();
+
+  const handleDisplayName = (e) => {
+    setNewDisplayName(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newDisplayName !== currentUser.displayName) {
+      await currentUser.updateProfile({ displayName: newDisplayName });
+      refreshCurrentUser();
+    }
+  };
+
+  useOnClickOutside(editFormRef, () => {
+    setIsEditBtnClick(false);
+  });
+  return (
+    <div className="edit-modal-container">
+      <form ref={editFormRef} onSubmit={handleSubmit} className="edit-modal">
+        <div className="edit-modal__row-1">
+          <AiIcons.AiOutlineClose className="close" />
+          <h3>Edit profile</h3>
+          <button type="submit" className="save">
+            Save
+          </button>
+        </div>
+
+        <div className="edit-modal__row-2">
+          <label htmlFor="bg-img">
+            <AiIcons.AiOutlineCamera className="camera" />
+            <AiIcons.AiOutlineClose className="close" />
+            <input type="file" accept="image/*" id="bg-img" className="bg-img" />
+          </label>
+        </div>
+
+        <div className="edit-modal__row-3">
+          <div className="profile-img">
+            {currentUser.profilePhoto ? (
+              <img src={currentUser.profilePhoto} alt="" />
+            ) : (
+              <div className="default-img"></div>
+            )}
+
+            <label htmlFor="profile-img">
+              <AiIcons.AiOutlineCamera className="camera" />
+            </label>
+            <input type="file" accept="image/*" id="profile-img" className="input" />
+          </div>
+        </div>
+
+        <div className="edit-modal__row-4">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            className="input"
+            value={newDisplayName}
+            onChange={handleDisplayName}
+          />
+        </div>
+
+        <div className="edit-modal__row-5">
+          <label htmlFor="bio">Bio</label>
+          <textarea type="text" id="name" className="input" />
+        </div>
+
+        <div className="edit-modal__row-6">
+          <label htmlFor="location">Location</label>
+          <input type="text" id="location" className="input" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileEditModal;
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { firebaseAuth } from '../firebase';
 import { useHistory } from 'react-router-dom';
 import ProfileMain from 'components/ProfileMain';
+import ProfileEditModal from 'components/ProfileEditModal';
 
-const Profile = ({ currentUser }) => {
+const Profile = ({ currentUser, refreshCurrentUser }) => {
   let history = useHistory();
   const [isEditBtnClick, setIsEditBtnClick] = useState(false);
 
@@ -21,6 +22,21 @@ const Profile = ({ currentUser }) => {
           setIsEditBtnClick={setIsEditBtnClick}
         />
       </div>
+      {isEditBtnClick ? (
+        <ProfileEditModal
+          currentUser={currentUser}
+          refreshCurrentUser={refreshCurrentUser}
+          setIsEditBtnClick={setIsEditBtnClick}
+        />
+      ) : null}
+      <nav>
+        <ul>
+          <li>Tweets</li>
+          <li>Tweets & replies</li>
+          <li>Media</li>
+          <li>Likes</li>
+        </ul>
+      </nav>
     </div>
   );
 };
