79de72bc3f5e7fe3e3c79377fb94617da31a0a27
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-11-05 14:49:16 +0000

    Update fixed sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,52 +6,60 @@ import * as BiIcons from 'react-icons/bi';
 import { FiHash } from 'react-icons/fi';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 import { CgMoreO } from 'react-icons/cg';
+import { HiUserCircle } from 'react-icons/hi';
 
 const SideBar = () => {
   return (
-    <nav className="sidebar">
-      <ul>
-        <li>
-          <Link to="/">
-            <FaIcons.FaTwitter className="twitter-icon" />
-          </Link>
-        </li>
-        <li>
-          <RiIcons.RiHome8Line />
-          <span> Home </span>
-        </li>
-        <li>
-          <FiHash />
-          <span> Explore </span>
-        </li>
-        <li>
-          <IoMdNotificationsOutline />
-          <span> Notification</span>
-        </li>
-        <li>
-          <BiIcons.BiEnvelope />
-          <span> Message</span>
-        </li>
-        <li>
-          <BiIcons.BiBookmark />
-          <span> Bookmarks </span>
-        </li>
-        <li>
-          <RiIcons.RiFileList2Line />
-          <span> Lists </span>
-        </li>
-        <li>
-          <Link to="/profile">
-            <FaIcons.FaUser />
-            <span>Profile</span>
-          </Link>
-        </li>
-        <li>
-          <CgMoreO />
-          <span> More</span>
-        </li>
-      </ul>
-    </nav>
+    <div className="sidebar-container">
+      <nav className="sidebar">
+        <ul>
+          <li>
+            <Link to="/">
+              <FaIcons.FaTwitter className="twitter-icon" />
+            </Link>
+          </li>
+          <li>
+            <RiIcons.RiHome8Line />
+            <span> Home </span>
+          </li>
+          <li>
+            <FiHash />
+            <span> Explore </span>
+          </li>
+          <li>
+            <IoMdNotificationsOutline />
+            <span> Notification</span>
+          </li>
+          <li>
+            <BiIcons.BiEnvelope />
+            <span> Message</span>
+          </li>
+          <li>
+            <BiIcons.BiBookmark />
+            <span> Bookmarks </span>
+          </li>
+          <li>
+            <RiIcons.RiFileList2Line />
+            <span> Lists </span>
+          </li>
+          <li>
+            <Link to="/profile">
+              <FaIcons.FaUser />
+              <span>Profile</span>
+            </Link>
+          </li>
+          <li>
+            <CgMoreO />
+            <span> More</span>
+          </li>
+
+          <li>
+            <HiUserCircle />
+            <span> More</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
